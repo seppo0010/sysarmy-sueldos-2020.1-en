32d7a85b4ac1473c61b78481ba818c9cebbb78a4
Sebastian Waisbrot <seppo0010@gmail.com>
2022-08-27 08:51:01 -0300

    Deploy

---
+++ precache-manifest.63be1ac9d6db7e5bdda12cc18eec83b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cb252ed8914ce14796d521f2121fa0",
+    "url": "/sysarmy-sueldos-2020.1-en/index.html"
+  },
+  {
+    "revision": "5a6ad05be85e23934aac",
+    "url": "/sysarmy-sueldos-2020.1-en/static/css/main.095e5f73.chunk.css"
+  },
+  {
+    "revision": "d09e5ef28e53acdfdbc0",
+    "url": "/sysarmy-sueldos-2020.1-en/static/js/2.571e151c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sysarmy-sueldos-2020.1-en/static/js/2.571e151c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6ad05be85e23934aac",
+    "url": "/sysarmy-sueldos-2020.1-en/static/js/main.1b348203.chunk.js"
+  },
+  {
+    "revision": "c27c08d9453c5966b3eb",
+    "url": "/sysarmy-sueldos-2020.1-en/static/js/runtime-main.8a54276c.js"
+  }
+]);
